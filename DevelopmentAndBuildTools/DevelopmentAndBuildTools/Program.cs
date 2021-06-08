3d9cd346eea07b438768f2249480fd99c38cfb9a
Kirill <kirillmoloch@gmail.com>
2021-06-08 17:56:52 +0300

    Add  MaxCountOfNonRepeatingSymbolsInARow method

--- DevelopmentAndBuildTools/DevelopmentAndBuildTools/Program.cs
+++ DevelopmentAndBuildTools/DevelopmentAndBuildTools/Program.cs
@@ -6,7 +6,40 @@ namespace DevelopmentAndBuildTools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MaxCountOfNonRepeatingSymbolsInARow(args[0]));
+        }
+
+        static int MaxCountOfNonRepeatingSymbolsInARow(string line)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                return 0;
+            }
+
+            int maxCount = 0;
+            int count = 0;
+            char previousSymbol = line[0];
+
+            foreach (char currentSymbol in line)
+            {
+                if (currentSymbol != previousSymbol)
+                {
+                    count++;
+                    previousSymbol = currentSymbol;
+                }
+                else
+                {
+                    count = 1;
+                    previousSymbol = currentSymbol;
+                }
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                }
+            }
+
+            return maxCount;
         }
     }
 }

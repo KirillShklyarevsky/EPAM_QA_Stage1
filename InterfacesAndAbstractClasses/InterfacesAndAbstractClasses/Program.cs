22e19b75f5b0176c6fd24ebcf7155ef1e8bfc886
Kirill <kirillmoloch@gmail.com>
2021-06-15 17:53:17 +0300

    Initial commit

---
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesAndAbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

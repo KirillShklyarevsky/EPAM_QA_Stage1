ca1d22df820c6534017455f34a26dded7d170402
Kirill <kirillmoloch@gmail.com>
2021-06-17 02:45:51 +0300

    Minor fix

--- OOP/OOP/VehicleEntryPoint.cs
+++ OOP/OOP/VehicleEntryPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace OOP
 {
@@ -6,31 +7,38 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Car car = new Car(5,
-                  new Engine(165, 2.2, EngineTypes.Petrol, "GCBDT-1091866"),
-                  new Chassis(4, "GX620U1", 500),
-                  new Transmission(TransmissionTypes.Manual, 5, "ZF"));
+            try
+            {
+                Car car = new Car(5,
+                      new Engine(165, 2.2, EngineTypes.Petrol, "GCBDT-1091866"),
+                      new Chassis(4, "GX620U1", 500),
+                      new Transmission(TransmissionTypes.Manual, 5, "ZF"));
 
-            Truck truck = new Truck(15000,
-                  new Engine(300, 10, EngineTypes.Diesel, "AABDT-1866"),
-                  new Chassis(6, "620U1", 20000),
-                  new Transmission(TransmissionTypes.Manual, 8, "EATOn"));
+                Truck truck = new Truck(15000,
+                      new Engine(300, 10, EngineTypes.Diesel, "AABDT-1866"),
+                      new Chassis(6, "620U1", 20000),
+                      new Transmission(TransmissionTypes.Manual, 8, "EATOn"));
 
-            Bus bus = new Bus(32,
-                  new Engine(180, 6, EngineTypes.Gas, "CCBDT-1091866"),
-                  new Chassis(8, "15620U1", 5000),
-                  new Transmission(TransmissionTypes.Automatic, 6, "Aisin"));
+                Bus bus = new Bus(32,
+                      new Engine(180, 6, EngineTypes.Gas, "CCBDT-1091866"),
+                      new Chassis(8, "15620U1", 5000),
+                      new Transmission(TransmissionTypes.Automatic, 6, "Aisin"));
 
-            Scooter scooter = new Scooter(5,
-                  new Engine(60, 0.9, EngineTypes.Electrical, "BBBDT-1091866"),
-                  new Chassis(2, "13420U1", 100),
-                  new Transmission(TransmissionTypes.Automatic, 4, "Jatco"));
+                Scooter scooter = new Scooter(5,
+                      new Engine(60, 0.9, EngineTypes.Electrical, "BBBDT-1091866"),
+                      new Chassis(2, "13420U1", 100),
+                      new Transmission(TransmissionTypes.Automatic, 4, "Jatco"));
 
-            List<VehicleBase> vehicles = new List<VehicleBase>() { bus, car, truck, scooter };
+                List<VehicleBase> vehicles = new List<VehicleBase>() { bus, car, truck, scooter };
 
-            Serializer<VehicleBase>.Serialize("VehiclesWithEngineCapacityMoreThan1,5.xml", Helper.VehiclesWithEngineCapacityMoreThan(1.5, vehicles));
-            Serializer<Engine>.Serialize("EngineTypeSerialNumberCapacityOfTruckAndBus.xml", Helper.EngineTypeSerialNumberCapacityOfTruckAndBus(vehicles));
-            Serializer<VehicleBase>.Serialize("GroupedByTransmission.xml", Helper.GroupedByTransmission(vehicles));
+                Serializer<VehicleBase>.Serialize("VehiclesWithEngineCapacityMoreThan1,5.xml", Helper.VehiclesWithEngineCapacityMoreThan(1.5, vehicles));
+                Serializer<Engine>.Serialize("EngineTypeSerialNumberCapacityOfTruckAndBus.xml", Helper.EngineTypeSerialNumberCapacityOfTruckAndBus(vehicles));
+                Serializer<VehicleBase>.Serialize("GroupedByTransmission.xml", Helper.GroupedByTransmission(vehicles));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }

dc6f15ae1a5e7383a0e66237cb09436b93259611
Kirill <kirillmoloch@gmail.com>
2021-09-08 17:44:46 +0300

    Add car class

---
+++ ObjectOrientedDesignPrinciples/ObjectOrientedDesignPrinciples/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace ObjectOrientedDesignPrinciples
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Number { get; set; }
+
+        public double CostOfOne { get; set; }
+
+        public Car(string brand, string model, int number, double costOfOne)
+        {
+            Brand = brand;
+            Model = model;
+            Number = number;
+            CostOfOne = costOfOne;
+        }
+    }
+}
